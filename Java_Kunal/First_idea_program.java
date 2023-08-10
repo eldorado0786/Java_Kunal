b1dc661a933e234496ee9406284ed6a5ee11f39b
eldorado0786 <mdatifraza926@gmail.com>
2023-08-10 10:41:25 +0530

    first commit

---
+++ Java_Kunal/First_idea_program.java
@@ -0,0 +1,53 @@
+package com.Java_Kunal;
+import java.util.Scanner;
+public class First_idea_program {
+    public static void main(String[] args) {
+
+//        1. for printing on the screen
+
+//        System.out.println("Hello World!");
+
+
+//        2.for taking input we have to import java.util.Scanner and write this code
+
+//        Scanner input = new Scanner(System.in);
+
+//        System.out.println("Enter the two  numbers to find its sum -> ");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        System.out.println("Sum of two number is "+(a+b));
+
+//        3.Primitive data type
+
+//        int roll_no = 64;
+//        char letter = 'r';
+//        float marks = 98.67f;
+//        double largeDecimalNumbers = 4567654.4567;
+//        long largeInteger = 34567834567886743L;
+//        boolean check = false;
+
+//        4.TypeCasting || Type Conversion
+
+
+//        Automatic Typecasting
+
+//        System.out.println("Automatic  type casting : ");
+//        float float_num = 43.54f;// inputting the float number
+//        float int_num = 43; // inputting the int number
+//        System.out.println("Printing the value of type float "+float_num);
+//        System.out.println("Printing the value of type int in the float variable  " + int_num);
+
+//       Narrowing conversion
+
+//        int num = (int) (67.56f);
+//        System.out.println("Narrowing conversion inserting the float value into the int data type variable : "+num);
+
+//        5.Automatic Type promotion in expressions
+
+//        int a = 257;
+//        byte b = (byte)(a);// 257 % 256 = 1
+//        System.out.println("Printing the data stored in the byte variable "+b);
+//        int number = 'A';
+//        System.out.println("Inserting the character in the int data type it is performing the automatic type promotion and printing the ascii key for it : "+number);
+    }
+}
---
+++ Java_Kunal/Introduction_to_Array_and_Arraylist.java
@@ -0,0 +1,62 @@
+package com.Java_Kunal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Introduction_to_Array_and_Arraylist {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        Syntax of the array
+//        datatype[] variable_name = new datatype[size];
+
+
+
+//        Q1:creating the array when the size is known
+//        int[] arr = new int[5];
+//        arr[0]=1;
+//        arr[1]=3;
+//        arr[2]=4;
+//        arr[3]=6;
+//        arr[4]=7;
+
+
+//        Q2:inputting the array when the size is not know
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = input.nextInt();
+//        }
+//        displaying the array
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i]+" ");
+//        }
+//        displaying all the array at once
+//        System.out.println(Arrays.toString(arr));
+
+
+
+//    Q3:2-d array
+//        System.out.println("2-D array ");
+//        System.out.println("Enter the number for the 2-D array ");
+//         int[][] arr = new int[3][3];
+//        for (int row = 0; row < arr.length ; row++) {
+//            for (int col = 0; col < arr[row].length ; col++) {
+//                arr[row][col]=input.nextInt();
+//            }
+//        }
+//        System.out.println(Arrays.deepToString(arr));
+
+//        Q4:Input array when the size is not known using the ArrayList
+
+        ArrayList <Integer> list = new ArrayList<>(10);//initial capacity is ignored if it reaches the limit
+
+        list.add(61);
+        list.add(6);
+        list.add(1);
+        list.add(4);
+        System.out.println(list.contains(6));
+        System.out.println(list);
+        list.set(2,100);
+        list.remove(3);
+        System.out.println(list);
+        System.out.println(list.get(2));
+    }
+}
