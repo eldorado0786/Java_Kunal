048d2d6f5fed3809c9bbe146111bf443dabadaaa
eldorado0786 <mdatifraza926@gmail.com>
2023-08-28 13:22:03 +0530

    initial commit in lecture

---
+++ Java_Kunal_Lec/Quick_sort_using_Recursion.java
@@ -0,0 +1,39 @@
+package com.Java_Kunal_Lec;
+
+import java.util.Arrays;
+
+public class Quick_sort_using_Recursion {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void sort(int[] nums, int low , int high){
+        if (low>=high){
+            return;
+        }
+        int s = low;
+        int e = high;
+        int m = s + (e-s)/2;
+        int pivot = nums[m];
+        while (s<=e){
+//            also a reason why if its already sorted it will not swap
+            while (nums[s]<pivot){
+                s++;
+            }
+            while (nums[e]>pivot){
+                e--;
+            }
+            if (s<=e){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+//        now my pivot is at correct index , please sort my two halves now
+        sort(nums,low,e);
+        sort(nums,s,high);
+    }
+}
