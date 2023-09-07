e84498859ab64b4bc6467b81ec6c83ec4daa882e
eldorado0786 <mdatifraza926@gmail.com>
2023-09-07 12:42:53 +0530

    initial commit in lecture oop

---
+++ Java_Kunal_Lec/OOP_4_Access_Control_In_built_Packages_Object_Class/access/Main.java
@@ -0,0 +1,10 @@
+package com.Java_Kunal_Lec.OOP_4_Access_Control_In_built_Packages_Object_Class.access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(34,"atif") ;
+        /* Needs to do a few things */
+//        1. access the data members
+//        2.modify the data members
+    }
+}
