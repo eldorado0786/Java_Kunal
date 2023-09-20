518d048a8aee072d17283cff1fbacfd6b60cedee
eldorado0786 <mdatifraza926@gmail.com>
2023-09-20 13:35:12 +0530

    initial commit in lecture linked list 1 hr complete

---
+++ Java_Kunal_Lec/linkedList/Main.java
@@ -0,0 +1,22 @@
+package com.Java_Kunal_Lec.linkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(10);
+        list.display();
+        list.insertLast(1);
+        list.display();
+        list.insert(100,3);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+    }
+}
