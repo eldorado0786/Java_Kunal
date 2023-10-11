ba5002eb0ba6410dcdb5f26a9f6c2e27be5fc275
eldorado0786 <mdatifraza926@gmail.com>
2023-10-11 11:31:43 +0530

    initial commit in lecture linked list

--- Leet_code/Palindrome_Linked_List_234.java
+++ Leet_code/Palindrome_Linked_List_234.java
@@ -12,49 +12,49 @@ public class Palindrome_Linked_List_234 {
      * }
      */
 
-        public boolean isPalindrome(ListNode head) {
-            ListNode mid = middleNode(head);
-            ListNode headSecond = reverseList(mid);
-
-            // Compare both the halves
-            while (head != null && headSecond != null) {
-                if (head.val != headSecond.val) {
-                    return false;
-                }
-                head = head.next;
-                headSecond = headSecond.next;
-            }
-            return true;
-        }
-
-        public ListNode middleNode(ListNode head) {
-            ListNode slow = head;
-            ListNode fast = head;
-
-            while (fast != null && fast.next != null) {
-                slow = slow.next;
-                fast = fast.next.next;
-            }
-
-            return slow;
-        }
-
-        public ListNode reverseList(ListNode head) {
-            if (head == null) {
-                return head;
-            }
-            ListNode prev = null;
-            ListNode current = head;
-
-            while (current != null) {
-                ListNode next = current.next;
-                current.next = prev;
-                prev = current;
-                current = next;
-            }
-
-            return prev;
-        }
-
+//        public boolean isPalindrome(ListNode head) {
+//            ListNode mid = middleNode(head);
+//            ListNode headSecond = reverseList(mid);
+//
+//            // Compare both the halves
+//            while (head != null && headSecond != null) {
+//                if (head.val != headSecond.val) {
+//                    return false;
+//                }
+//                head = head.next;
+//                headSecond = headSecond.next;
+//            }
+//            return true;
+//        }
+//
+//        public ListNode middleNode(ListNode head) {
+//            ListNode slow = head;
+//            ListNode fast = head;
+//
+//            while (fast != null && fast.next != null) {
+//                slow = slow.next;
+//                fast = fast.next.next;
+//            }
+//
+//            return slow;
+//        }
+//
+//        public ListNode reverseList(ListNode head) {
+//            if (head == null) {
+//                return head;
+//            }
+//            ListNode prev = null;
+//            ListNode current = head;
+//
+//            while (current != null) {
+//                ListNode next = current.next;
+//                current.next = prev;
+//                prev = current;
+//                current = next;
+//            }
+//
+//            return prev;
+//        }
+//
 
 }
