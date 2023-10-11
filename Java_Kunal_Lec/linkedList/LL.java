f4a9665cb4e6c591a091a3cbc74d89a4acb550da
eldorado0786 <mdatifraza926@gmail.com>
2023-10-11 11:31:24 +0530

    initial commit in lecture linked list

--- Java_Kunal_Lec/linkedList/LL.java
+++ Java_Kunal_Lec/linkedList/LL.java
@@ -6,48 +6,48 @@ public class LL {
     private Node tail;
     private int size;
 
-    public  LL() {
-        this.size=0;
+    public LL() {
+        this.size = 0;
     }
 
-    public int deleteFirst(){
+    public int deleteFirst() {
         int val = head.val;
         head = head.next;
-        if (head==null){
-            tail=null;
+        if (head == null) {
+            tail = null;
         }
         size--;
         return val;
     }
 
-    public int deleteLast(){
-        if (size<=1){
+    public int deleteLast() {
+        if (size <= 1) {
             return deleteFirst();
         }
-        Node secondLast = get(size-2);
+        Node secondLast = get(size - 2);
         int val = tail.val;
-        tail=secondLast;
+        tail = secondLast;
         tail.next = null;
         return val;
     }
 
-    public int delete(int index){
-        if (index==0){
-          return   deleteFirst();
+    public int delete(int index) {
+        if (index == 0) {
+            return deleteFirst();
         }
-        if (index==size-1){
-          return   deleteLast();
+        if (index == size - 1) {
+            return deleteLast();
         }
-        Node prev = get(index-1);
+        Node prev = get(index - 1);
         int val = prev.next.val;
         prev.next = prev.next.next;
         return val;
     }
 
-    public Node find_the_node_which_has_the_value(int val){
+    public Node find_the_node_which_has_the_value(int val) {
         Node node = head;
-        while (node!=null){
-            if (node.val==val){
+        while (node != null) {
+            if (node.val == val) {
                 return node;
             }
             node = node.next;
@@ -55,7 +55,7 @@ public class LL {
         return null;
     }
 
-    public Node get(int index){
+    public Node get(int index) {
         Node node = head;
         for (int i = 0; i < index; i++) {
             node = node.next;
@@ -63,12 +63,12 @@ public class LL {
         return node;
     }
 
-    public void insert(int val,int index){
-        if (index==0){
+    public void insert(int val, int index) {
+        if (index == 0) {
             insertFirst(val);
             return;
         }
-        if (index==size){
+        if (index == size) {
             insertLast(val);
             return;
         }
@@ -76,21 +76,22 @@ public class LL {
         for (int i = 1; i < index; i++) {
             temp = temp.next;
         }
-        Node node = new Node(val,temp.next);
-        temp.next=node;
+        Node node = new Node(val, temp.next);
+        temp.next = node;
         size++;
     }
 
-    public void display(){
+    public void display() {
         Node temp = head;
-        while (temp!=null){
-            System.out.print(temp.val+" -> ");
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
             temp = temp.next;
         }
         System.out.println("END");
     }
-    public void insertLast(int val){
-        if (tail==null){
+
+    public void insertLast(int val) {
+        if (tail == null) {
             insertFirst(val);
             return;
         }
@@ -99,18 +100,20 @@ public class LL {
         tail = node;
         size++;
     }
-    public void insertFirst(int val){
+
+    public void insertFirst(int val) {
         Node node = new Node(val);
         node.next = head;
-        head=node;
-        if (tail==null){
-            tail=head;
+        head = node;
+        if (tail == null) {
+            tail = head;
         }
-        size+=1;
+        size += 1;
     }
-    private class Node{
-      private int val;
-       private Node next;
+
+    private class Node {
+        private int val;
+        private Node next;
 
         public Node(int val) {
             this.val = val;
@@ -122,13 +125,14 @@ public class LL {
         }
     }
 
-//    Inserting using the recursion after the index
-    public void insertRec(int val,int index){
-        head = insertRec(val,index,head);
+    //    Inserting using the recursion after the index
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
     }
-    private Node insertRec(int val,int index,Node node){
-        if (index==0){
-            Node temp = new Node(val,node);
+
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
             size++;
             return temp;
         }
@@ -137,44 +141,60 @@ public class LL {
 
     }
 
-//    Leet code question 83
-    public void duplicates(){
-        Node node = head ;
-        while (node.next!=null){
-            if (node.val==node.next.val){
-                node.next=node.next.next;
+    //    Leet code question 83
+    public void duplicates() {
+        Node node = head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
                 size--;
-            }else {
+            } else {
                 node = node.next;
             }
         }
         tail = node;
         tail.next = null;
     }
-// Leet code Question 21
-    public static LL merge(LL first,LL second){
+
+    // Leet code Question 21
+    public static LL merge(LL first, LL second) {
         Node f = first.head;
         Node s = second.head;
         LL ans = new LL();
-        while (f!=null&&s!=null){
-            if (f.val<s.val){
+        while (f != null && s != null) {
+            if (f.val < s.val) {
                 ans.insertLast(f.val);
-                f=f.next;
-            }else {
+                f = f.next;
+            } else {
                 ans.insertLast(s.val);
-                s=s.next;
+                s = s.next;
             }
         }
-        while (f!=null){
+        while (f != null) {
             ans.insertLast(f.val);
-            f=f.next;
+            f = f.next;
         }
-        while (s!=null){
+        while (s != null) {
             ans.insertLast((s.val));
-            s=s.next;
+            s = s.next;
         }
         return ans;
     }
+
+
+    //        Recursion reverse
+    private void reverse(Node node) {
+        if (node == tail) {
+            head = tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+
+//    time: 1:56 hr
     public static void main(String[] args) {
 //        LL list = new LL();
 //        Leet Code Question 83
@@ -210,5 +230,10 @@ public class LL {
 //        second.insertLast(14);
 //        LL ans = LL.merge(first,second);
 //        ans.display();
+
+//    Reversing the linked list
+
+
     }
+
 }
---
+++ Leet_code/Reverse_Linked_List_206.java
@@ -0,0 +1,21 @@
+package com.Leet_code;
+
+public class Reverse_Linked_List_206 {
+//    public ListNode reverseList(ListNode head) {
+//        if(head==null){
+//            return head;
+//        }
+//        ListNode prev = null;
+//        ListNode present = head;
+//        ListNode next = present.next;
+//        while(present!=null){
+//            present.next = prev;
+//            prev = present;
+//            present = next;
+//            if (next!=null){
+//                next = next.next;
+//            }
+//        }
+//        return prev ;
+//    }
+}
